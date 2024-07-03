90c8df0711bfd5015882f1636c78862f76f7d325
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2024-07-03 11:00:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, Welcome to CodeMastermindHQ! </h1>
+      <h3>
+        My name is Ajay Dhangar and I shared wih you my live code playground.
+      </h3>
+    </div>
+  );
+}
